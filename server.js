78be5e9c8561932cd732e945e81fda5d26302f39
ChinmayeePrasad327 <pvrbchinmayee@gmail.com>
2026-06-16 15:12:04 +0530

    Intital Project Setup

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./src/config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("PrintFlow Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
